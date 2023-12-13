14d581e192a3c9c4eaa54fe5d3218aee348bdc1c
k a l l i (ɔ◔‿◔)ɔ ♥ <152912045+kalliannhale@users.noreply.github.com>
2023-12-12 19:29:59 -0500

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec 10 20:54:33 2023
-
-@author: kalliann
+file - main.py
+    demonstrates usage
 """
 
 from community import Community
